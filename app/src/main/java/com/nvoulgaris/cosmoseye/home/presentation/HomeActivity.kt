c7fac466539a57aba9879f89d4a198be4414ed67
nvoulgaris <nvoulgaris@vivantehealth.com>
2018-12-15 17:28:04 +0200

    Introduce bottom navigation scaffolding

--- app/src/main/java/com/nvoulgaris/cosmoseye/home/presentation/HomeActivity.kt
+++ app/src/main/java/com/nvoulgaris/cosmoseye/home/presentation/HomeActivity.kt
@@ -1,9 +1,12 @@
 package com.nvoulgaris.cosmoseye.home.presentation
 
+import android.os.Bundle
 import com.nvoulgaris.cosmoseye.R
 import com.nvoulgaris.cosmoseye.application.CosmosEyeApplication
 import com.nvoulgaris.cosmoseye.base.injection.modules.ActivityModule
 import com.nvoulgaris.cosmoseye.base.presentation.BaseInjectingActivity
+import kotlinx.android.synthetic.main.home_activity.*
+import timber.log.Timber
 
 class HomeActivity : BaseInjectingActivity<HomeActivityComponent>() {
 
@@ -15,5 +18,21 @@ class HomeActivity : BaseInjectingActivity<HomeActivityComponent>() {
         val app = application as CosmosEyeApplication
         val activityModule = ActivityModule(this)
         return app.getComponent().createHomeActivityComponent(activityModule)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        attachNavigationListener()
+    }
+
+    private fun attachNavigationListener() {
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_navigation_apod -> Timber.e("APOD tab")
+                R.id.bottom_navigation_gallery -> Timber.e("Gallery tab")
+                R.id.bottom_navigation_mars_rover -> Timber.e("Mars rover tab")
+            }
+            true
+        }
     }
 }
