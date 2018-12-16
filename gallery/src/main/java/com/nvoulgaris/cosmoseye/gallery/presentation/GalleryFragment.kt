2da9314242a0e4d4cd83f16d7fbf7e95e0b005fe
nvoulgaris <nvoulgaris@vivantehealth.com>
2018-12-16 11:00:12 +0200

    Gallery search layout scaffolding

--- gallery/src/main/java/com/nvoulgaris/cosmoseye/gallery/presentation/GalleryFragment.kt
+++ gallery/src/main/java/com/nvoulgaris/cosmoseye/gallery/presentation/GalleryFragment.kt
@@ -1,8 +1,13 @@
 package com.nvoulgaris.cosmoseye.gallery.presentation
 
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import com.nvoulgaris.cosmoseye.base.gallery.R
 import com.nvoulgaris.cosmoseye.base.presentation.BaseInjectingActivity
 import com.nvoulgaris.cosmoseye.base.presentation.BaseInjectingFragment
-import cosmoseye.nvoulgaris.com.base.gallery.R
+import kotlinx.android.synthetic.main.gallery_fragment.*
+import timber.log.Timber
 
 class GalleryFragment : BaseInjectingFragment() {
 
@@ -13,4 +18,23 @@ class GalleryFragment : BaseInjectingFragment() {
     }
 
     override fun getLayoutId(): Int = R.layout.gallery_fragment
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        attachListeners()
+    }
+
+    private fun attachListeners() {
+        gallery_search_input.setOnTouchListener { _, event ->
+            val DRAWABLE_RIGHT = 2;
+
+            if(event.action == MotionEvent.ACTION_UP) {
+                if(event.rawX >= (gallery_search_input.right - gallery_search_input.compoundDrawables[DRAWABLE_RIGHT].bounds.width())) {
+                    Timber.e("aaaaaaaaaaaaaaaa")
+                    gallery_search_input.text.clear()
+                }
+            }
+            false
+        }
+    }
 }
